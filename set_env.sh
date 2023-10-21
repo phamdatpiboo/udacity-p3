c896ed0952c1da7b94017f213ccdf9ce4e53be17
bolobala <phamdat@PHAMnoMacBook-Pro.local>
2023-10-21 13:00:41 +0900

    version 2

---
+++ set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Ph0ngdat
+#export POSTGRES_HOST=datpt57-p3.cgi1c9fyyfgz.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacityproject3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
