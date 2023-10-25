43e13925f66fb50467926f1f63c1e5ebef6a36df
PHAM TIEN DAT <datpt57@fpt.com>
2023-10-25 23:11:59 +0900

    Fix err

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a43e85b8e570b4cd4b9094088ea395f9-1698807221.us-east-1.elb.amazonaws.co:8080/api/v0'
+  apiHost: 'http://a43e85b8e570b4cd4b9094088ea395f9-1698807221.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
