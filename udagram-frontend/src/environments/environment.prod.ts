651dee5be35f0306b6b6b280403a6df919ec42a4
PHAM TIEN DAT <datpt57@fpt.com>
2023-10-25 23:08:48 +0900

    change latest3

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://adce68ba11bdd4389b71b6ed0b3e8669-734460086.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a43e85b8e570b4cd4b9094088ea395f9-1698807221.us-east-1.elb.amazonaws.co:8080/api/v0'
 };
 
 /*
