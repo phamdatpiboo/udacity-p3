fd5321cf71daa78069078023c1ad349258ef78c0
PHAM TIEN DAT <datpt57@fpt.com>
2023-10-25 22:29:10 +0900

    push to address

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://adce68ba11bdd4389b71b6ed0b3e8669-734460086.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
